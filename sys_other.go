d992d60733644e99c47ff7b2d3bed572a68c06af
Ivan Sorokin <unxed@mail.ru>
2026-05-30 12:51:45 +0200

    Fix build

--- sys_other.go
+++ sys_other.go
@@ -5,11 +5,6 @@ package zip
 
 import "os"
 
-type hardlinkKey struct{}
-
-func getHardLinkTarget(fi os.FileInfo, seen map[hardlinkKey]string) string { return "" }
-func rememberHardLink(fi os.FileInfo, relPath string, seen map[hardlinkKey]string) {}
-
 func sysPlatformExtra(fi os.FileInfo, hdr *FileHeader) {}
 func extractSpecialFile(path string, hdr *FileHeader) error { return nil }
 func sysXattrs(path string, hdr *FileHeader) error { return nil }
