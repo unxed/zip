dbccccda85ff3d0acb924bf12393a2d5309ec285
Ivan Sorokin <unxed@mail.ru>
2026-06-13 17:06:38 +0200

    Minor

--- sys_other.go
+++ sys_other.go
@@ -9,6 +9,3 @@ func sysPlatformExtra(fi os.FileInfo, hdr *FileHeader) {}
 func extractSpecialFile(path string, hdr *FileHeader) error { return nil }
 func sysXattrs(path string, hdr *FileHeader) error { return nil }
 func applyXattrs(path string, hdr *FileHeader) error { return nil }
-func createWindowsSymlink(target, link string, isDir bool) error {
-	return nil // No-op, never called on non-Windows
-}
